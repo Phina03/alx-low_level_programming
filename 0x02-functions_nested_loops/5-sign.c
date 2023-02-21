a1c37db9c178876bf4594c06f759e884d6f44056
Phina03 <seraphycangel@gmail.com>
2023-02-21 14:20:09 -0800

    printed numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - Print sign of a number
+ * @n: Number to be printed
+ * Return: 1 || 0 || -1
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
+
+
+
