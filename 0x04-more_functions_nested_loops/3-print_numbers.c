cce7173bf824bd6474a3fcf3f770c8f61c12dbf9
Phina03 <seraphycangel@gmail.com>
2023-02-23 22:27:12 -0800

    print numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers from 0 - 9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
