20827f01696448bc3457a087861d7f3b5cc0fdbc
Phina03 <seraphycangel@gmail.com>
2023-03-13 15:45:18 -0700

    static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _puts - Function to be printed
+ * @s: Value to be printed
+ * Return: Always 0
+ */
+void _puts(char *s)
+{
+	puts(s);
+}
